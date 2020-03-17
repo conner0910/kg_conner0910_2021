09514107f58f2223245d6e92f0f495f49717fb08
Conner Zeigman <conner0910@gmail.com>
2020-03-17 17:12:35 -0400

    First working code that works with examples given. Further testing to be done

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 function main(str1, str2){
-    
+    let mapping = {};
+    if(str1.length !== str2.length){
+        return false;
+    }
+    for(i = 0; i < str1.length; i++){
+        let char1 = str1.charAt(i);
+        let char2 = str2.charAt(i);
+        if(Object.keys(mapping).includes(char1)){
+            if(Object.keys(mapping)[char1] !== char2){
+                return false;
+            }
+        }
+        else{
+            mapping[char1] = char2;
+        }        
+    }
+    return true;
 }
 
-main(process.argv[2], process.argv[3]);
+console.log(main(process.argv[2], process.argv[3]));
